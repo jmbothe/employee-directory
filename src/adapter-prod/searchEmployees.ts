4f42719675d76129175641184ba5138d4e79da7b
Jeff Bothe <jeffrymbothe@gmail.com>
2021-05-01 17:21:56 -0400

    refactor: clean up prod adapter employee search error handling

--- src/adapter-prod/searchEmployees.ts
+++ src/adapter-prod/searchEmployees.ts
@@ -1,8 +1,4 @@
-import {
-  ApolloClient,
-  ApolloError,
-  NormalizedCacheObject
-} from "@apollo/client";
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
 import { SearchEmployeesResult } from "../domain/SearchEmployeesResult";
 import { EmployeeDto } from "./EmployeeDto";
 import searchEmployeesQuery from "./searchEmployeesQuery";
@@ -22,32 +18,27 @@ export default async function searchEmployees(
   name: string,
   page: number
 ): Promise<SearchEmployeesResult> {
-  const { data, errors } = await gqlClient.query<QueryResult>({
-    variables: { name, page },
-    query: searchEmployeesQuery,
-    // allows reading/handling `errors` property from response object.
-    errorPolicy: "all",
-  });
+  try {
+    const { data } = await gqlClient.query<QueryResult>({
+      variables: { name, page },
+      query: searchEmployeesQuery,
+    });
 
-  // handle empty results.
-  if (errors) {
-    if (errors.some(({ message }) => message === "404: Not Found")) {
+    const {characters} = data;
+    const {info, results} = characters;
+
+    return {
+      ...info,
+      employees: results.map(toEmployee),
+    };
+  } catch (error) {
+    if (error.message === "404: Not Found") {
       return {
         pages: 1,
         employees: [],
       };
     }
 
-    // Just do what Apollo would have done if we didn't set errorPolicy: "all"
-    // TODO: Improve error handling for other error scenarios.
-    throw new ApolloError({ graphQLErrors: errors });
+    throw error;
   }
-
-  // Apollo cache does not cache errors for errorPolicy: "all", so still need a null check for results.
-  // Appears to be a known issue.
-  // https://github.com/apollographql/apollo-client/issues/4644
-  return {
-    pages: data.characters?.info.pages ?? 1,
-    employees: (data.characters?.results ?? []).map(toEmployee),
-  };
 }
