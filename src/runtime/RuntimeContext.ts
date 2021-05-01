5ab7f9ebce4780f3e9130030696443ddf8182e77
Jeff Bothe <jeffrymbothe@gmail.com>
2021-05-01 17:40:27 -0400

    fix: add null check to useRuntime

--- src/runtime/RuntimeContext.ts
+++ src/runtime/RuntimeContext.ts
@@ -1,9 +1,6 @@
 import { createContext } from 'react';
 import { Runtime } from '../domain/Runtime';
 
-// Runtime is defined dependent on host env, i.e. dev/prod.
-// Prefer to ignore this one error, rather than turn off strict null checks everywhere.
-// @ts-ignore
-const RuntimeContext = createContext<Runtime>(undefined);
+const RuntimeContext = createContext<Runtime | undefined>(undefined);
 
 export default RuntimeContext;
--- src/runtime/useRuntime.ts
+++ src/runtime/useRuntime.ts
@@ -1,6 +1,13 @@
 import { useContext } from 'react';
+import { Runtime } from '../domain/Runtime';
 import RuntimeContext from './RuntimeContext';
 
-export default function useRuntime() {
-    return useContext(RuntimeContext);
+export default function useRuntime(): Runtime {
+    const runtime = useContext(RuntimeContext);
+
+    if (!runtime) {
+        throw new Error("No runtime was found in App context.");
+    }
+
+    return runtime;
 }
